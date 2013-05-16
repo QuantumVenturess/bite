bf65724570868487204f197f724fe7b4e1100170
Tommy Dang <quantumventuress@gmail.com>
2013-05-15 21:11:39 -0700

    Initial Commit

---
+++ BiteTests/BiteTests.h
@@ -0,0 +1,13 @@
+//
+//  BiteTests.h
+//  BiteTests
+//
+//  Created by Tommy DANGerous on 5/15/13.
+//  Copyright (c) 2013 Quantum Ventures. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BiteTests : SenTestCase
+
+@end
---
+++ BiteTests/BiteTests.m
@@ -0,0 +1,32 @@
+//
+//  BiteTests.m
+//  BiteTests
+//
+//  Created by Tommy DANGerous on 5/15/13.
+//  Copyright (c) 2013 Quantum Ventures. All rights reserved.
+//
+
+#import "BiteTests.h"
+
+@implementation BiteTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in BiteTests");
+}
+
+@end
